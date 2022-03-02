7ca35f8fb3838961d76968fd0e66552425c57dc5
chige12 <tige1215.technic@gmail.com>
2022-03-02 18:02:46 +0900

    ➕ add package

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,19 @@ module.exports = {
     browser: true,
     node: true,
   },
+  "parser": "vue-eslint-parser",
+  "parserOptions": {
+    "parser": "@typescript-eslint/parser"
+  },
   extends: [
     '@nuxtjs/eslint-config-typescript',
     'plugin:nuxt/recommended',
     'prettier',
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:vue/recommended",
+    "@vue/prettier",
+    "@vue/typescript"
   ],
   plugins: [],
   // add your custom rules here
