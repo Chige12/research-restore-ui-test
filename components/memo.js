d56ed54bf3397c6bc6e031bd78e212773fe523fd
chige12 <tige1215.technic@gmail.com>
2022-03-02 18:03:39 +0900

    📝 add program memo

---
+++ components/memo.js
@@ -0,0 +1,22 @@
+const obj = {
+  lastname: ['Doe',  { firstname: ['John'] }]
+};
+
+const chainedKeyText = 'lastname.1.firstname.0'
+const chainedKey = chainedKeyText.split('.')
+
+let value = { ...obj }
+for ( const key of chainedKey ) {
+  value = value[key]
+}
+
+console.log(chainedKeyText, value) // > "lastname.1.firstname.0" "John"
+
+let diffObj = value
+for ( const key of chainedKey.reverse()) {
+  let newObj = {}
+  newObj[key] = diffObj
+  diffObj = { ...newObj }
+}
+
+console.log(diffObj) // > { lastname: { 1: { firstname: { 0: "John" } } } }
