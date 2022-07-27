66e5bc240779a1fe306a6ae80a21dda74ee2d34f
chige12 <tige1215.technic@gmail.com>
2022-07-27 14:40:04 +0900

    ♻️  外部関数化 cssProperties

--- pages/checkPercent.vue
+++ pages/checkPercent.vue
@@ -41,12 +41,11 @@
 
 <script>
 import Vue from 'vue'
-import { get, cloneDeep } from 'lodash'
 import json1 from '@/assets/json/diffHistories-signin-comp02.json'
 import json2 from '@/assets/json/diffHistories-signin-comp04.json'
 import { diff as justDiff } from 'just-diff'
 import { getNewRootPathElements } from '@/utils/getNewRootPathElements'
-const cssProperties = require('css-properties')
+import { countChanges, filteredCssProperties } from '@/utils/cssPropeties'
 
 export default {
   name: 'checkPercent',
@@ -101,7 +100,10 @@ export default {
       const obj = JSON.parse(JSON.stringify(json));
       const { allElementStylesPerDiff } = obj
 
-      this.openCssPropeties(allElementStylesPerDiff)
+      this.allCssProperties = filteredCssProperties.length
+
+      const styleDiffs = getStyleDiffs(allElementStylesPerDiff)
+      this.countedProperties = getCountedProperties(styleDiffs)
     },
     openDiffHistories(diffHistories) {
       for (let i = 0; i < diffHistories.length; i++) {
@@ -122,110 +124,11 @@ export default {
         }
         const infos = diffHistories[i].diffAndInfos;
         if (!infos) continue;
-        infos.forEach(info => {
-          switch (info.type) {
-            case 'class':
-            case 'style':
-              this.changeStyleCount++
-              break;
-            case 'dom':
-              this.domChanges(info.elementDiffs)
-            default:
-              break;
-          }
-        });
-      }
-    },
-    openCssPropeties(allElementStylesPerDiff) {
-      const styleDiffs = []
-      for (let i = 0; i < allElementStylesPerDiff.length - 1; i++) {
-        const props1 = allElementStylesPerDiff[i];
-        const props2 = allElementStylesPerDiff[i + 1];
-        const diffs = justDiff(props1, props2)
-        const filteredDiffs = diffs.filter((diff) => {
-          return diff.op === 'replace'
-        })
-        const diffAndStyle = filteredDiffs.map((diff) => {
-          const { path } = diff
-          const lastPath = path.slice(0, path.length - 1)
-          const style = get(props2, lastPath)
-          return {
-            ...diff,
-            property: style.property
-          }
-        })
-        styleDiffs.push(...diffAndStyle)
-        console.log(styleDiffs)
-      }
-      console.log('styleDiffs',styleDiffs)
-
-      const filteredCssProperties = cssProperties.filter((prop) => {
-        switch (prop) {
-          case 'border-bottom':
-          case 'border-left':
-          case 'border-right':
-          case 'border-top':
-          case 'border-bottom-color':
-          case 'border-left-color':
-          case 'border-right-color':
-          case 'border-top-color':
-          case 'text-decoration-color':
-          case 'column-rule-color':
-          case 'outline-color':
-          case 'margin':
-          case 'padding':
-          case 'border-top-left-radius':
-          case 'border-top-right-radius':
-          case 'border-bottom-left-radius':
-          case 'border-bottom-right-radius':
-          case 'overflow':
-          case 'border-bottom-width':
-          case 'border-top-width':
-          case 'border-right-width':
-          case 'border-left-width':
-          case 'border-bottom-style':
-          case 'border-left-style':
-          case 'border-right-style':
-          case 'border-top-style':
-          case 'list-style-type':
-            return false
-          default:
-            return true
-            break;
-        }
-      })
-
-      this.allCssProperties = filteredCssProperties.length
-      const countedProperties = filteredCssProperties.map((property) => {
-        const filteredStyleDiffs = styleDiffs.filter((diff) => {
-          return diff.property === property
-        })
-        const count = filteredStyleDiffs.length
-        return {
-          property,
-          count,
-        }
-      })
-      this.countedProperties = countedProperties
-    },
-    domChanges(elementDiffs) {
-      const to = elementDiffs.to
-      const from = elementDiffs.from
-      if (to === undefined && from !== undefined) {
-        this.removeDomCount++
-        return;
-      }
-      if (to !== undefined && from === undefined) {
-        this.addDomCount++
-        return;
-      }
-      if (to !== undefined && from !== undefined) {
-        if (to.tagName === from.tagName) {
-          this.changeDomCount++
-          return;
-        }
-        this.addDomCount++
-        this.removeDomCount++
+        const changes = countChanges(infos)
+        this.changeStyleCount = changes.changeStyleCount
+        this.addDomCount = changes.addDomCount
+        this.deleteDomCount = changes.deleteDomCount
+        this.changeDomCount = changes.changeDomCount
       }
     }
   }
---
+++ utils/cssPropeties.ts
@@ -0,0 +1,139 @@
+import { diff as justDiff } from 'just-diff'
+import { get } from 'lodash'
+import {
+  CSSStyle,
+  Diff,
+  DiffAndInfos,
+  ElementDiffs,
+  elementStyle,
+} from '~/mixins/deepDiffType'
+const cssProperties = require('css-properties') as string[]
+
+export type DiffAndStyle = Diff & {
+  property: CSSStyle['property']
+}
+
+export type StyleDiffs = DiffAndStyle[][]
+
+export const filteredCssProperties = cssProperties.filter((prop) => {
+  switch (prop) {
+    case 'border-bottom':
+    case 'border-left':
+    case 'border-right':
+    case 'border-top':
+    case 'border-bottom-color':
+    case 'border-left-color':
+    case 'border-right-color':
+    case 'border-top-color':
+    case 'text-decoration-color':
+    case 'column-rule-color':
+    case 'outline-color':
+    case 'margin':
+    case 'padding':
+    case 'border-top-left-radius':
+    case 'border-top-right-radius':
+    case 'border-bottom-left-radius':
+    case 'border-bottom-right-radius':
+    case 'overflow':
+    case 'border-bottom-width':
+    case 'border-top-width':
+    case 'border-right-width':
+    case 'border-left-width':
+    case 'border-bottom-style':
+    case 'border-left-style':
+    case 'border-right-style':
+    case 'border-top-style':
+    case 'list-style-type':
+      return false
+    default:
+      return true
+  }
+})
+
+export const getCountedProperties = (styleDiffs: StyleDiffs) => {
+  const countedProperties = filteredCssProperties.map((property) => {
+    const filteredStyleDiffs = styleDiffs.filter((diff) => {
+      return diff.filter((x) => x.property === property)
+    })
+    const count = filteredStyleDiffs.length
+    return {
+      property,
+      count,
+    }
+  })
+  return countedProperties
+}
+
+export const getStyleDiffs = (
+  allElementStylesPerDiff: elementStyle[][]
+): StyleDiffs => {
+  const styleDiffs = [] as StyleDiffs
+  for (let i = 0; i < allElementStylesPerDiff.length - 1; i++) {
+    const props1 = allElementStylesPerDiff[i]
+    const props2 = allElementStylesPerDiff[i + 1]
+    const diffs = justDiff(props1, props2)
+    const filteredDiffs = diffs.filter((diff) => {
+      return diff.op === 'replace'
+    })
+    const diffAndStyle: DiffAndStyle[] = filteredDiffs.map((diff: Diff) => {
+      const { path } = diff
+      const lastPath = path.slice(0, path.length - 1)
+      const style: CSSStyle = get(props2, lastPath)
+      return {
+        ...diff,
+        property: style.property,
+      }
+    })
+    styleDiffs.push(diffAndStyle)
+    console.log(styleDiffs)
+  }
+  return styleDiffs
+}
+
+export const countChanges = (infos: DiffAndInfos) => {
+  let removeDomCount: number = 0
+  let addDomCount: number = 0
+  let changeDomCount: number = 0
+  let changeStyleCount: number = 0
+
+  infos.forEach((info) => {
+    switch (info.type) {
+      case 'class':
+      case 'style':
+        changeStyleCount++
+        break
+      case 'dom':
+        info.elementDiffs && domChanges(info.elementDiffs)
+      default:
+        break
+    }
+  })
+
+  const domChanges = (elementDiffs: ElementDiffs) => {
+    const to = elementDiffs.to
+    const from = elementDiffs.from
+    if (to === undefined && from !== undefined) {
+      removeDomCount++
+      return
+    }
+    if (to !== undefined && from === undefined) {
+      addDomCount++
+      return
+    }
+    if (to !== undefined && from !== undefined) {
+      if ('tagName' in to && 'tagName' in from && to.tagName === from.tagName) {
+        changeDomCount++
+        return
+      }
+      addDomCount++
+      removeDomCount++
+    }
+  }
+
+  return {
+    removeDomCount,
+    addDomCount,
+    changeDomCount,
+    changeStyleCount,
+  }
+}
