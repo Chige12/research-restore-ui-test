d82e5c52e56ffe426af7fe454d8fbffa2a906a43
chige12 <tige1215.technic@gmail.com>
2022-08-23 16:26:59 +0900

    ✨ use element ui

--- pages/signin/comp03.vue
+++ pages/signin/comp03.vue
@@ -1,16 +1,47 @@
 <template>
-  <v-container>
-    <v-row justify="center" align="center">
-      <v-col cols="12" sm="8" md="6">
-        <v-card id="check-component" color="#fafafa" elevation="0">
-          <h3>サインイン</h3>
-          <label>メール：<input type="email" /></label>
-          <label>パスワード：<input type="password" /></label>
-          <input type="submit" value="サインイン" />
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <el-container>
+    <el-main>
+      <el-row type="flex" class="row-bg" justify="center">
+        <el-col :span="6">
+          <el-form
+            :model="ruleForm"
+            status-icon
+            :rules="rules"
+            id="check-component"
+            ref="ruleForm"
+          >
+            <h3 class="mb-4">サインイン</h3>
+            <el-form-item
+              label="E-mail"
+              :label-width="formLabelWidth"
+              prop="email"
+            >
+              <el-input
+                v-model="ruleForm.email"
+                type="email"
+                placeholder="Please input email"
+                autocomplete="off"
+              />
+            </el-form-item>
+            <el-form-item
+              label="Password"
+              :label-width="formLabelWidth"
+              prop="password"
+            >
+              <el-input
+                v-model="ruleForm.password"
+                type="password"
+                placeholder="Please input password"
+                show-password
+                autocomplete="off"
+              />
+            </el-form-item>
+            <el-button type="primary" @click="toLink">サインイン</el-button>
+          </el-form>
+        </el-col>
+      </el-row>
+    </el-main>
+  </el-container>
 </template>
 
 <script lang="ts">
@@ -18,34 +49,66 @@ import Vue from 'vue'
 import Mixin from '~/mixins/deepDiff'
 
 type Data = {
-  email: string
-  password: string
   isShowPassword: boolean
+  ruleForm: {
+    email: string
+    password: string
+  }
   rules: {
     required: (value: string) => string | boolean
     min: (v: string) => string | boolean
-    email: (value: string) => string | boolean
+    email: any
+    password: any
   }
+  formLabelWidth: string
 }
 
 export default Vue.extend({
   name: 'SignInComp01',
   mixins: [Mixin],
   data(): Data {
+    const validateEmail = (rule: any, value: string, callback: any) => {
+      const pattern =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      if (value === '') {
+        callback(new Error('Please input the email'))
+      } else if (!pattern.test(value)) {
+        callback(new Error('Invalid e-mail.'))
+      } else {
+        callback()
+      }
+    }
+
+    const validatePass = (rule: any, value: string, callback: any) => {
+      if (value === '') {
+        callback(new Error('Please input the password'))
+      } else {
+        if (value.length < 8) {
+          callback(new Error('Min 8 characters'))
+        }
+        callback()
+      }
+    }
+
     return {
-      email: '',
-      password: '',
       isShowPassword: false,
+      ruleForm: {
+        email: '',
+        password: '',
+      },
       rules: {
+        email: [{ validator: validateEmail, trigger: 'change' }],
+        password: [{ validator: validatePass, trigger: 'change' }],
         required: (value) => !!value || 'Required.',
         min: (v) => v.length >= 8 || 'Min 8 characters',
-        email: (value) => {
-          const pattern =
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-          return pattern.test(value) || 'Invalid e-mail.'
-        },
       },
+      formLabelWidth: '80px',
     }
   },
+  methods: {
+    toLink() {
+      this.$router.push('/inspire')
+    }
+  }
 })
 </script>
