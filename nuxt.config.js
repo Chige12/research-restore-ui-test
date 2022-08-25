5319e3248629e5886100ea74764ac72f85ff25b6
chige12 <tige1215.technic@gmail.com>
2022-08-25 15:04:37 +0900

    ➕ add package iview

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,7 @@ export default defineNuxtConfig({
   css: ['element-ui/lib/theme-chalk/index.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['@/plugins/element-ui'],
+  plugins: ['@/plugins/element-ui', '@/plugins/iview'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
---
+++ plugins/iview.js
@@ -0,0 +1,7 @@
+import iview from 'view-design'
+import Vue from 'vue'
+
+// import style
+import 'view-design/dist/styles/iview.css'
+
+Vue.use(iview)
