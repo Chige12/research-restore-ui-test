32dc4a7917687bd779962689e7a70d5be171647e
chige12 <tige1215.technic@gmail.com>
2023-01-22 03:55:44 +0900

    ✨ refactor matchingClass

--- utils/guessCombination/matchingClass.ts
+++ utils/guessCombination/matchingClass.ts
@@ -12,9 +12,11 @@ export class Matching {
     Matching.useIndicatorName = useIndicatorName
     Matching.cacheIndicatorIndex = 0
     Matching.matchingArr = []
-    Matching.XeventIdArr = []
-    Matching.YeventIdArr = []
-    Matching.prevArrIndicatorValue = null
+    Matching.fixedOperationTargetIdsX = []
+    Matching.fixedOperationTargetIdsY = []
+    Matching.fixedOperationIndexesX = []
+    Matching.fixedOperationIndexesY = []
+    Matching.prevIndicatorValue = null
     Matching.correctCount = 0
     Matching.allCount = 0
   }
@@ -22,9 +24,11 @@ export class Matching {
   static useIndicatorName: string = ''
   static matchingArr: CombinationWithIndicator[] = []
   static cacheIndicatorIndex: number = 0
-  static XeventIdArr: string[] = []
-  static YeventIdArr: string[] = []
-  static prevArrIndicatorValue: number | null = null
+  static fixedOperationTargetIdsX: string[] = []
+  static fixedOperationTargetIdsY: string[] = []
+  static fixedOperationIndexesX: number[] = []
+  static fixedOperationIndexesY: number[] = []
+  static prevIndicatorValue: number | null = null
   static correctCount: number = 0
   static allCount: number = 0
 
@@ -40,46 +44,103 @@ export class Matching {
       combinationWithIndicators
     )
 
-    for (let i = 0; i < sortedArr.length; i++) {
-      const comb = sortedArr[i]
-      const [histFileX, histFileY] = comb.combination
-      if (Matching.isAdoptMatching(histFileX, histFileY)) {
-        const judge = Matching.judge(histFileX, histFileY)
-        const ableToJudge: boolean | undefined = true
-        const adoptedComb = { ...comb, judge, ableToJudge }
-        Matching.matchingArr.push(adoptedComb)
-        Matching.pushArrAddedId(comb)
-        Matching.allCount++
-        if (judge) {
-          Matching.correctCount++
+    const filteredArr = sortedArr.filter((x) => {
+      const [fileX, fileY] = x.combination
+      return fileX.history.eventInfo.type === fileY.history.eventInfo.type
+    })
+
+    for (let i = 0; i < filteredArr.length; i++) {
+      const comb = filteredArr[i]
+      const [fileX, fileY] = comb.combination
+
+      const nowIndicatorValue =
+        comb.indicator.values[Matching.cacheIndicatorIndex].number
+      const isSameAsPrevIndicatorValue =
+        nowIndicatorValue === Matching.prevIndicatorValue
+      const isFirst = i === 0
+
+      //最初なら飛ばす
+      if (!isFirst) {
+        // 指標が前の周回の値と違うなら
+        if (isSameAsPrevIndicatorValue) {
+          // 前の周回の操作番号と操作対象を固定化
+          const prevMatch =
+            Matching.matchingArr[Matching.matchingArr.length - 1]
+          const [fileX, fileY] = prevMatch.combination
+          Matching.fixedOperationIndexesX.push(fileX.index)
+          Matching.fixedOperationIndexesY.push(fileY.index)
+          Matching.fixedOperationTargetIdsX.push(
+            fileX.history.eventInfo.eventId
+          )
+          Matching.fixedOperationTargetIdsY.push(
+            fileY.history.eventInfo.eventId
+          )
         }
       }
+
+      // 既に固定化されてる操作番号 or 操作対象なら追加しない
+      if (Matching.isAlreadyFixedOperation(fileX, fileY)) continue
+
+      // 追加処理
+      const judge = Matching.judge(fileX, fileY)
+      const ableToJudge: boolean | undefined = true // とりあえずtrue
+      const adoptedComb = { ...comb, judge, ableToJudge }
+      Matching.matchingArr.push(adoptedComb)
+      Matching.allCount++
+      if (judge) {
+        Matching.correctCount++
+      }
+
+      Matching.prevIndicatorValue = nowIndicatorValue
     }
-    Matching.prevArrIndicatorValue = null
+    Matching.prevIndicatorValue = null
+
+    // 重複してる指標を判定不可に設定
+    const index = Matching.cacheIndicatorIndex
+    const matchings = Matching.matchingArr.map((x, _i, self) => {
+      const indicatorValue = x.indicator.values[index].number
+      const firstIndex = self.findIndex(
+        (s) => s.indicator.values[index].number === indicatorValue
+      )
+      const firstReverseIndex = self
+        .concat()
+        .reverse()
+        .findIndex((s) => s.indicator.values[index].number === indicatorValue)
+      const lastIndex = self.length - 1 - firstReverseIndex
+      const isNotDuplicate = firstIndex === lastIndex
+      return {
+        ...x,
+        ableToJudge: isNotDuplicate as boolean | undefined,
+      }
+    })
+
     return {
-      matchings: Matching.matchingArr,
+      matchings,
       allCount: Matching.allCount,
       correctCount: Matching.correctCount,
     }
   }
 
-  // static sameIndicatorValueCount = (arr: CombinationWithIndicator[]) => {
-  //   const indicatorArr = arr.map(
-  //     (combi) => combi.indicator.values[Matching.indicatorIndex].number
-  //   )
-  //   var count = {}
-
-  //   for (var i = 0; i < indicatorArr.length; i++) {
-  //     const number = indicatorArr[i]
-  //     count[number] = (count[number] || 0) + 1
-  //   }
-
-  //   console.log(count)
-  //   return s.size != arr.length
-  // }
-
-  // static isNeedReMatching = (arr: CombinationWithIndicator[]) => {
-  // }
+  static isAlreadyFixedOperation = (
+    fileX: HistoryAndFileData,
+    fileY: HistoryAndFileData
+  ) => {
+    const isFixedOpIndexX = Matching.fixedOperationIndexesX.some(
+      (index) => index === fileX.index
+    )
+    const isFixedOpIndexY = Matching.fixedOperationIndexesY.some(
+      (index) => index === fileY.index
+    )
+    const isFixedOpTargetIdX = Matching.fixedOperationTargetIdsX.some(
+      (id) => id === fileX.history.eventInfo.eventId
+    )
+    const isFixedOpTargetIdY = Matching.fixedOperationTargetIdsY.some(
+      (id) => id === fileY.history.eventInfo.eventId
+    )
+    const isAlreadyFixedOpIndex = isFixedOpIndexX || isFixedOpIndexY
+    const isAlreadyFixedOpTargetId = isFixedOpTargetIdX || isFixedOpTargetIdY
+    return isAlreadyFixedOpIndex && isAlreadyFixedOpTargetId
+  }
 
   static judge = (X: HistoryAndFileData, Y: HistoryAndFileData): boolean => {
     const nameAndOpNumX = fileNameToAlphabet(X.fileName).split('_')
@@ -145,25 +206,11 @@ export class Matching {
     }
   }
 
-  static pushArrAddedId = (comb: CombinationWithIndicator) => {
-    const [histFileX, histFileY] = comb.combination
+  static updatePrevJudgement = () => {
     const index = Matching.cacheIndicatorIndex
-    const isSameIndicatorToPrev =
-      Matching.prevArrIndicatorValue === comb.indicator.values[index].number
-    Matching.prevArrIndicatorValue = comb.indicator.values[index].number
-
-    if (!isSameIndicatorToPrev) {
-      Matching.XeventIdArr.push(histFileX.history.eventInfo.eventId)
-      Matching.YeventIdArr.push(histFileY.history.eventInfo.eventId)
-    } else {
-      Matching.updatePrevJudgement(index)
-    }
-  }
-
-  static updatePrevJudgement = (index: number) => {
     const updateIndex = Matching.matchingArr.findIndex(
       (match) =>
-        match.indicator.values[index].number === Matching.prevArrIndicatorValue
+        match.indicator.values[index].number === Matching.prevIndicatorValue
     )
     const ableToJudge: boolean | undefined = false
     Matching.matchingArr[updateIndex] = {
@@ -174,23 +221,5 @@ export class Matching {
       ...Matching.matchingArr[Matching.matchingArr.length - 1],
       ableToJudge,
     }
-  }
-
-  static isAdoptMatching = (
-    historyAndFileDataX: HistoryAndFileData,
-    historyAndFileDataY: HistoryAndFileData
-  ): boolean => {
-    const isAEventIdExist = Matching.XeventIdArr.some((id: string) => {
-      return id === historyAndFileDataX.history.eventInfo.eventId
-    })
-    const isBEventIdExist = Matching.YeventIdArr.some((id: string) => {
-      return id === historyAndFileDataY.history.eventInfo.eventId
-    })
-
-    const isSameEventType =
-      historyAndFileDataX.history.eventInfo.type ===
-      historyAndFileDataY.history.eventInfo.type
-
-    return !isAEventIdExist && !isBEventIdExist && isSameEventType
   }
 }
