a5368f8d1d87f5686221fde433666e9a9df09665
chige12 <tige1215.technic@gmail.com>
2023-02-09 15:57:17 +0900

    ⚡️ 遷移先ページを用意

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,14 +1,8 @@
 <template>
   <v-row>
     <v-col class="text-center">
-      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
       <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
+        &#8220; ページの遷移先として用意したページです。 &#8221;
       </blockquote>
     </v-col>
   </v-row>
